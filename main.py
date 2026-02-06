fce307a4db4ab8791140c9c569650eddc67ecc75
Shizhe Liu <69354000+ShizheL@users.noreply.github.com>
2026-02-06 23:06:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/add")
-def add(x, y):
-    return {"result": (x + y)}
+def add(x: int, y: int):
+    return {"x": x, "y": y, "result": x + y}
