930423270246a87a97140b0fda80261df9d44564
Shizhe Liu <69354000+ShizheL@users.noreply.github.com>
2026-02-07 04:19:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class AddRequest(BaseModel):
+    x: int
+    y: int
 
 @app.get("/add")
 def add(x: int, y: int):
     return {"x": x, "y": y, "result": x + y}
+
+@app.post("/add")
+def add_post(req: AddRequest):
+    return {"x": req.x, "y": req.y, "result": req.x + req.y}
